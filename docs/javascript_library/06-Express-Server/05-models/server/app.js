e3cecd73c7cf2ff182ec89fa7a51293da0a14df7
Paul O'Connor <jamespauloconnor@gmail.com>
2018-03-13 09:03:21 -0400

    Reordering of folder structure

---
+++ docs/javascript_library/06-Express-Server/05-models/server/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// app.use('/api/cors/error', function(req, res){
+// 	res.send("This is an endpoint that won't arrive in another domain because of cors errors.");
+// });
+
+// app.use(require('./middleware/headers'));
+
+//http://localhost:3000/somedata.json
+app.use(express.static('data'))
+
+app.use('/api/test', function(req, res){
+	res.send("This is data from the /api/test endpoint. It's from the server.");
+});
+
+app.use('/api/userdata', function(req, res){
+	res.send({ user: 'kenn' });
+});
+
+app.use('/api/somejsondata', function(req, res){
+	res.sendFile('./somedata.json')
+})
+app.listen(3000, function(){
+	console.log('App is listening on 3000.')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
