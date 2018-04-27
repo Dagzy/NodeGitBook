eb84a75a1fd072cdbdf9202f5aa67900f9699291
Aaron Ofengender <aofengen@gmail.com>
2018-04-27 11:57:19 -0400

    aaron - server side code set up for querying DB

--- docs/javascript_library/06-Express-Server/13-advanced-sequelize/completedCode/server/app.js
+++ docs/javascript_library/06-Express-Server/13-advanced-sequelize/completedCode/server/app.js
@@ -4,16 +4,18 @@ var express = require('express');
 var app = express();
 var testRoutes = require('./controllers/testcontroller')
 var userRoutes = require('./controllers/user')
+var logRoutes = require('./controllers/logcontroller');
 var sequelize = require('./db');
 var bodyParser = require('body-parser');
 
 sequelize.sync(); // tip: {force: true} for resetting tables
 
 app.use(bodyParser.json());
-// app.use(require('./middleware/headers'));
+app.use(require('./middleware/headers'));
 // app.use(require('./middleware/validate-session'));
 
 app.use('/api/user', userRoutes);
+app.use('/api/log', logRoutes);
 
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
